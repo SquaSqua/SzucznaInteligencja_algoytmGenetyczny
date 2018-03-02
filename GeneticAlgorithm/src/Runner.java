9be519152d1f7bfd7432b851641c74a963a5d8ca
SquaSqua <ewaskorska94@gmail.com>
2018-03-02 12:03:01 +0100

    1

---
+++ GeneticAlgorithm/src/Runner.java
@@ -0,0 +1,8 @@
+import runners.MSRCPSPDERunner;
+
+public class Runner {
+    public static void main(String[] args)
+    {
+        MSRCPSPDERunner msrcpspdeRunner = new MSRCPSPDERunner();
+    }
+}
