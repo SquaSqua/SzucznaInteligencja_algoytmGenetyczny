615b0ee0517c78f833a063ccdfbedd40bf49b0dd
SquaSqua <ewaskorska94@gmail.com>
2018-03-12 01:38:30 +0100

    początek ewolucji, zero testów
    mutacja, crossowanie, nie wiadomo co działa

--- GeneticAlgorithm/src/Population.java
+++ GeneticAlgorithm/src/Population.java
@@ -1,31 +1,38 @@
+import msrcpsp.evaluation.BaseEvaluator;
+import msrcpsp.evaluation.DurationEvaluator;
 import msrcpsp.io.MSRCPSPIO;
 import msrcpsp.scheduling.Resource;
 import msrcpsp.scheduling.Schedule;
 import msrcpsp.scheduling.Task;
 import msrcpsp.scheduling.schedule_builders.ForwardScheduleBuilder;
 import msrcpsp.scheduling.schedule_builders.ScheduleBuilder;
-
 import java.util.List;
 import java.util.Random;
 
 public class Population
 {
     Schedule[] population;
-    int pop_size;
-    int cross_prob;
-    int mut_prob;
+    private int generations;
+    private int pop_size;
+    private double cross_prob;
+    private double mut_prob;
+    private int tournamentSize;
+    private String definitionFile;
 
-    public Population(int pop_size, int cross_prob, int mut_prob)
+    public Population(String definitionFile, int generations, int pop_size, double cross_prob, double mut_prob, int tournamentSize)
     {
+        this.generations = generations;
         this.pop_size = pop_size;
         population = new Schedule[pop_size];
         this.cross_prob = cross_prob;
         this.mut_prob = mut_prob;
+        this.tournamentSize = tournamentSize;
+        this.definitionFile = definitionFile;
     }
 
-    public void initialize(String definitionFile)
+    public void initialize()
     {
-        MSRCPSPIO reader= new MSRCPSPIO();
+        MSRCPSPIO reader = new MSRCPSPIO();
         Schedule sch = reader.readDefinition(definitionFile);
         for(int i = 0; i < pop_size; i++)
         {
@@ -33,10 +40,8 @@ public class Population
             schedule(sch2);
             ScheduleBuilder builder = new ForwardScheduleBuilder(sch2.getSuccesors());
             builder.buildTimestamps(sch2);
-//            System.out.println(sch.getTasks()[0] + " + " + sch.getTasks()[0].getResourceId());
             population[i] = sch2;
         }
-//        showPopulation();
     }
 
     public void showPopulation()
@@ -69,7 +74,7 @@ public class Population
         }
     }
 
-    public void schedule(Schedule schedule)
+    private void schedule(Schedule schedule)
     {
         Random rand = new Random();
         Task[] tasks = schedule.getTasks();
@@ -81,20 +86,97 @@ public class Population
         }
     }
 
-    public Schedule[] crossing_over(Schedule sch1, Schedule sch2)//tu będę zwracać tablicę dwóch Scheduli
+    public Schedule[] crossing_over(Schedule sch1, Schedule sch2)//w ogóle nijak nie sprawdzone XD
     {
         //check if there will be a crossing
         Random rand = new Random();
+        Schedule[] result;
         if(rand.nextDouble() < cross_prob)
         {
             //if yes, then find an index of crossing point
+            int cross_ind = rand.nextInt(sch1.getTasks().length);
+            Schedule schedule1 = new Schedule(sch1);
+            Schedule schedule2 = new Schedule(sch2);
+            for(int i = 0; i < cross_ind; i ++)
+            {
+                schedule1.getTasks()[i].setResourceId(sch1.getTasks()[i].getResourceId());
+                schedule2.getTasks()[i].setResourceId(sch2.getTasks()[i].getResourceId());
+            }
+            for(int i = cross_ind; i < schedule1.getTasks().length; i ++)
+            {
+                schedule1.getTasks()[i].setResourceId(sch2.getTasks()[i].getResourceId());
+                schedule2.getTasks()[i].setResourceId(sch1.getTasks()[i].getResourceId());
+            }
+            ScheduleBuilder builder1 = new ForwardScheduleBuilder(schedule1.getSuccesors());
+            builder1.buildTimestamps(schedule1);
+            ScheduleBuilder builder2 = new ForwardScheduleBuilder(schedule2.getSuccesors());
+            builder2.buildTimestamps(schedule2);
 
+            //return those 2 crossed individuals in an array
+            result = new Schedule[]{schedule1, schedule2};
         }
-
-
-
-        //return those 2 crossed individuals in an array
-
         //if no then return 2 parents in an array
+        result = new Schedule[]{sch1, sch2};
+
+        return result;
+    }
+
+    public Schedule mutation(Schedule sch1)//to też nie było sprawdzane
+    {
+        Random rand = new Random();
+        Schedule schedule = new Schedule(sch1);
+        for(int i = 0; i < sch1.getTasks().length; i++)
+        {
+            if(rand.nextDouble() < mut_prob)
+            {
+                Task task = sch1.getTasks()[i];
+                List<Resource> capRes = sch1.getCapableResources(task);
+                schedule.getTasks()[i].setResourceId(capRes.get(rand.nextInt(capRes.size())).getId());
+            }
+            else
+            {
+                schedule.getTasks()[i].setResourceId(sch1.getTasks()[i].getResourceId());
+            }
+        }
+        ScheduleBuilder builder = new ForwardScheduleBuilder(schedule.getSuccesors());
+        builder.buildTimestamps(schedule);
+        return schedule;
+    }
+
+    public Schedule tournament(int tournamentSize)//co ja robię ze swoim kodem
+    {
+        Schedule[] tournament = new Schedule[tournamentSize];
+        Schedule winner;
+        double lowestDuration;
+        Random random = new Random();
+        for(int i = 0; i < tournamentSize; i++)
+        {
+            tournament[i] = new Schedule(population[random.nextInt(pop_size)]);
+        }
+        winner = tournament[0];
+        BaseEvaluator evaluator1 = new DurationEvaluator(winner);
+        lowestDuration = evaluator1.evaluate();
+        if(tournamentSize > 1)
+        {
+            for(int i = 1; i < tournamentSize; i++)
+            {
+                BaseEvaluator evaluatorTemp = new DurationEvaluator(tournament[i]);
+                if(lowestDuration > evaluatorTemp.evaluate())
+                {
+                    lowestDuration = evaluatorTemp.evaluate();
+                    winner = tournament[i];
+                }
+            }
+        }
+        return winner;
+    }
+
+    public void evolution()//w ogóle gdzie to ma być? może w Runnerze?
+    {
+        initialize();
+
+
+        //crossing_over
+        //evolution
     }
 }
--- GeneticAlgorithm/src/Runner.java
+++ GeneticAlgorithm/src/Runner.java
@@ -1,14 +1,9 @@
-import runners.MSRCPSPGARunner;
-
-import java.util.logging.Logger;
-
-public class Runner {
-
-    private static final Logger LOGGER = Logger.getLogger(MSRCPSPGARunner.class.getName());
+public class Runner
+{
     private static final String definitionFile = "C:/Users/Ewa Skórska/Documents/GitHub/SzucznaInteligencja_algoytmGenetyczny/GeneticAlgorithm/src/dataset_def/100_5_22_15.def";
     public static void main(String[] args)
     {
-        Population pop = new Population(100);
-        pop.initialize(definitionFile);
+        Population pop = new Population(definitionFile, 1000, 100, 2, 2, 5);
+        pop.evolution();
     }
 }
